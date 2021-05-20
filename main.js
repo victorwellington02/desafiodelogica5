bd29f598d9b33c4321f05aeb1ca024ec49d0295f
victorwellington02 <wellington.vlnunes02@gmail.com>
2021-05-19 22:30:29 -0300

    Desafio Finalizado

---
+++ main.js
@@ -0,0 +1,33 @@
+//Desafio 5 - número 1
+
+let itens = ["Monitor","PenDrive","Mouse","Teclado","MousePad"]
+
+console.log(itens)
+
+//Desafio 5 - número 2
+
+itens.unshift("Headset")
+
+//Desafio 5 - número 3
+
+itens.shift("Headset")
+
+//Desafio 5 - número 4
+
+itens.push("HdExterno")
+
+itens.push("MouseBungee")
+
+//Desafio 5 - número 5
+
+itens.pop("HdExterno")
+
+itens.pop("MouseBungee")
+
+//Desafio 5 - número 6
+
+let cont = [7,5,6,3,8,9,2,1,4]
+
+cont.sort()
+
+console.log(cont)
